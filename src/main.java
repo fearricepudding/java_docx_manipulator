d0218a4bae8bf8629aa6025c96ed0d2864ddbc2c
FEARricepudding <jordanrandles@googlemail.com>
2022-03-13 22:13:22 +0000

    Ew tabs

--- src/main.java
+++ src/main.java
@@ -24,77 +24,77 @@ import com.sun.star.comp.loader.JavaLoader;
 import java.nio.file.Paths;
 
 class docman{
-	public static void main(String[] args) throws IOException{
-		System.out.println("DOCMAN");
+    public static void main(String[] args) throws IOException{
+        System.out.println("DOCMAN");
 
-		Map<String, String> attributes = new HashMap<String, String>();
-		attributes.put("number", "22");
-		attributes.put("text", "Something New Here");
+        Map<String, String> attributes = new HashMap<String, String>();
+        attributes.put("number", "22");
+        attributes.put("text", "Something New Here");
 
-		String updatedDoc = replacePlaceholders("template.docx", attributes);
-		String pdf = convertToPDF(updatedDoc);
-	}
+        String updatedDoc = replacePlaceholders("template.docx", attributes);
+        String pdf = convertToPDF(updatedDoc);
+    }
 
-	public static String convertToPDF(String file){
-		File inputFile = new File(file);
-		String newName = file.replace(".docx", ".pdf");
-		File outputFile = new File(newName);
+    public static String convertToPDF(String file){
+        File inputFile = new File(file);
+        String newName = file.replace(".docx", ".pdf");
+        File outputFile = new File(newName);
 
-		//final RemoteOfficeManager officeManager = RemoteOfficeManager.install("127.0.0.1");
-		final LocalOfficeManager officeManager = LocalOfficeManager
-													//.officeHome("")
-													//.build();
-													.install();
-		try {
-			officeManager.start();
+        //final RemoteOfficeManager officeManager = RemoteOfficeManager.install("127.0.0.1");
+        final LocalOfficeManager officeManager = LocalOfficeManager
+                                                    //.officeHome("")
+                                                    //.build();
+                                                    .install();
+        try {
+            officeManager.start();
 
-			JodConverter
-					 .convert(inputFile)
-					 .to(outputFile)
-					 .execute();
-		} catch(OfficeException e){
-			e.printStackTrace();
-		} finally {
-			OfficeUtils.stopQuietly(officeManager);
-		}
-		return newName;
-	}
+            JodConverter
+                     .convert(inputFile)
+                     .to(outputFile)
+                     .execute();
+        } catch(OfficeException e){
+            e.printStackTrace();
+        } finally {
+            OfficeUtils.stopQuietly(officeManager);
+        }
+        return newName;
+    }
 
-	public static String replacePlaceholders(String template, Map<String, String> data) throws IOException{
-		String tempLocation = unzip(template);
-		String contentPath = tempLocation+"/word/document.xml";
-		String content = getFileContent(contentPath);
+    public static String replacePlaceholders(String template, Map<String, String> data) throws IOException{
+        String tempLocation = unzip(template);
+        String contentPath = tempLocation+"/word/document.xml";
+        String content = getFileContent(contentPath);
 
-		for(Map.Entry<String, String> entry : data.entrySet()){
-			String selector = "\\["+entry.getKey()+"\\]";
-			String value = entry.getValue();
-			content = content.replaceAll(selector, value);
-		}
+        for(Map.Entry<String, String> entry : data.entrySet()){
+            String selector = "\\["+entry.getKey()+"\\]";
+            String value = entry.getValue();
+            content = content.replaceAll(selector, value);
+        }
 
-		updateFileContent(contentPath, content);
-		Path toZip = Paths.get(tempLocation);
-		String newDocName = "updated.docx"; // replace with random name
-		zipFolder(toZip, newDocName);
-		deleteDir(tempLocation);
-		return newDocName;
-	}
+        updateFileContent(contentPath, content);
+        Path toZip = Paths.get(tempLocation);
+        String newDocName = "updated.docx"; // replace with random name
+        zipFolder(toZip, newDocName);
+        deleteDir(tempLocation);
+        return newDocName;
+    }
 
-	public static void updateFileContent(String filePath, String fileContent) throws IOException{
-		Path file = Paths.get(filePath);
-		Files.write(file, fileContent.getBytes());
-	}
+    public static void updateFileContent(String filePath, String fileContent) throws IOException{
+        Path file = Paths.get(filePath);
+        Files.write(file, fileContent.getBytes());
+    }
 
-	public static String getFileContent(String filePath){
-		String content = "";
-		try{
-			content = new String ( Files.readAllBytes( Paths.get(filePath) ) );
-		}catch (IOException e){
-			e.printStackTrace();
-		}
-		return content;
-	}
+    public static String getFileContent(String filePath){
+        String content = "";
+        try{
+            content = new String ( Files.readAllBytes( Paths.get(filePath) ) );
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return content;
+    }
 
-	public static void zipFolder(Path source, String zipFileName) throws IOException {
+    public static void zipFolder(Path source, String zipFileName) throws IOException {
         try(ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFileName))) {
             Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
                 @Override
@@ -125,64 +125,64 @@ class docman{
         }
     }
 
-	public static void deleteDir(String path){
-		File source = new File(path);
-		File[] content = source.listFiles();
-		if(content != null){
-			for(File f : content){
-				if(!Files.isSymbolicLink(f.toPath())){
-					deleteDir(f.toString());
-				}
-			}
-		}
-		source.delete();
-	}
+    public static void deleteDir(String path){
+        File source = new File(path);
+        File[] content = source.listFiles();
+        if(content != null){
+            for(File f : content){
+                if(!Files.isSymbolicLink(f.toPath())){
+                    deleteDir(f.toString());
+                }
+            }
+        }
+        source.delete();
+    }
 
-	public static String randomDir(){
-		byte[] array = new byte[7]; // length is bounded by 7
-		new Random().nextBytes(array);
-		String generatedString = new String(array, Charset.forName("UTF-8"));
-		return generatedString;
-	}
+    public static String randomDir(){
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-8"));
+        return generatedString;
+    }
 
-	public static String unzip(String fileZip) throws IOException{
-		String dest = "./tmp/"+randomDir();
+    public static String unzip(String fileZip) throws IOException{
+        String dest = "./tmp/"+randomDir();
         File destDir = new File(dest);
         byte[] buffer = new byte[1024];
         ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
         ZipEntry zipEntry = zis.getNextEntry();
         while (zipEntry != null) {
-			File newFile = newFile(destDir, zipEntry);
-			 if (zipEntry.isDirectory()) {
-				 if (!newFile.isDirectory() && !newFile.mkdirs()) {
-					 throw new IOException("Failed to create directory " + newFile);
-				 }
-			 } else {
-				 File parent = newFile.getParentFile();
-				 if (!parent.isDirectory() && !parent.mkdirs()) {
-					 throw new IOException("Failed to create directory " + parent);
-				 }
-				 FileOutputStream fos = new FileOutputStream(newFile);
-				 int len;
-				 while ((len = zis.read(buffer)) > 0) {
-					 fos.write(buffer, 0, len);
-				 }
-				 fos.close();
-			 }
-			 zipEntry = zis.getNextEntry();
+            File newFile = newFile(destDir, zipEntry);
+             if (zipEntry.isDirectory()) {
+                 if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                     throw new IOException("Failed to create directory " + newFile);
+                 }
+             } else {
+                 File parent = newFile.getParentFile();
+                 if (!parent.isDirectory() && !parent.mkdirs()) {
+                     throw new IOException("Failed to create directory " + parent);
+                 }
+                 FileOutputStream fos = new FileOutputStream(newFile);
+                 int len;
+                 while ((len = zis.read(buffer)) > 0) {
+                     fos.write(buffer, 0, len);
+                 }
+                 fos.close();
+             }
+             zipEntry = zis.getNextEntry();
         }
         zis.closeEntry();
         zis.close();
-		return dest;
-	};
+        return dest;
+    };
 
-	public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
-		File destFile = new File(destinationDir, zipEntry.getName());
-		String destDirPath = destinationDir.getCanonicalPath();
-		String destFilePath = destFile.getCanonicalPath();
-		if (!destFilePath.startsWith(destDirPath + File.separator)) {
-			throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
-		}
-		return destFile;
-	}
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+        return destFile;
+    }
 }
